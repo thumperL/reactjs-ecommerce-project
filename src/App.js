b753ed6f4a639b0d47237a0f2cf5bcff61659e6f
thumperL <16928657+thumperL@users.noreply.github.com>
2021-11-23 21:38:28 +1100

    add collection page and demo content
    including shop page, and the collection list and item component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shoppage/shoppage.component.jsx';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
   );
