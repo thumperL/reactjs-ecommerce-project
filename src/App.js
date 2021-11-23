b186f393be6dec64d145c431ac2b4129fe6477e9
thumperL <16928657+thumperL@users.noreply.github.com>
2021-11-23 21:59:43 +1100

    add header to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shoppage/shoppage.component.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
