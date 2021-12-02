ea08288e78ec8a5ef2160d1c3208870f5edac612
thumperL <16928657+thumperL@users.noreply.github.com>
2021-12-02 20:48:05 +1100

    patch Google signin button conflict with email signin button

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -55,7 +55,7 @@ class SingIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
             {' '}
             Sign in with Google {' '}
             </CustomButton>
